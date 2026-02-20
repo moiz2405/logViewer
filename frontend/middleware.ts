51162da1c58090d92341ba6fbeff00a47addf246
Moiz2405 <km856307@gmail.com>
2026-02-20 13:12:28 +0530

    Update frontend/middleware.ts

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,7 +1,6 @@
 import { auth } from "./auth";
 
 export default auth((req) => {
-  // Allow unauthenticated access to the homepage, login page, and NextAuth API routes
   const publicRoutes = ["/auth/sign-in"];
   if (
     !req.auth &&
@@ -9,15 +8,14 @@ export default auth((req) => {
     !req.nextUrl.pathname.startsWith("/api/auth")
   ) {
     const newUrl = new URL("/auth/sign-in", req.nextUrl.origin);
+    newUrl.searchParams.set("callbackUrl", req.nextUrl.href);
     return Response.redirect(newUrl);
   }
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
